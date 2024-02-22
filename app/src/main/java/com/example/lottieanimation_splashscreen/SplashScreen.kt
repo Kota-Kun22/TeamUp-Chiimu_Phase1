63cf1bea3191079d2744ff38516b29f4507bcadd
harsh <harshrastogi013@gmail.com>
2024-02-23 01:30:52 +0530

    added animation in text, popBackStack , navigation to Onboarding

--- app/src/main/java/com/example/lottieanimation_splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/lottieanimation_splashscreen/SplashScreen.kt
@@ -1,5 +1,7 @@
 package com.example.lottieanimation_splashscreen
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -10,22 +12,41 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
+
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController:NavHostController)
 {
+    val alpha = remember{
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true)
+    {
+        alpha.animateTo(
+            1f,
+            animationSpec = tween(
+                durationMillis = 4000
+            )
+        )
+        delay(3000)
+        navController.popBackStack()
+        navController.navigate("Onboarding")
+    }
     Column(/** sorry for this much space but i like this way*/
         modifier= Modifier
             .fillMaxSize()
@@ -40,7 +61,7 @@ fun SplashScreen(navController:NavHostController)
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
-        Text(text = "WELCOME TO CHIIMU",fontSize = 28.sp)
+        Text(text = "WELCOME TO CHIIMU",fontSize = 28.sp, modifier=Modifier.alpha(alpha.value))
 
         Spacer(modifier=Modifier.height(15.dp))
 
@@ -48,7 +69,10 @@ fun SplashScreen(navController:NavHostController)
 
         Spacer(modifier=Modifier.height(15.dp))
 
-        Text(text = "Lets Build you a Team !!...", fontSize = 28.sp)
+        Text(
+            text = "Lets Build you a Team !!...",
+            fontSize = 28.sp,
+            modifier=Modifier.alpha(alpha.value))
 
     }
 
